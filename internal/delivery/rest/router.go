ec9cdb80d076d6dc6d9e690031c47392779523a0
Jonathan Aaron Wibawa <aaron.wibawa@gmail.com>
2023-09-29 12:44:20 +0700

    added GetOrderInfo method for handler

--- internal/delivery/rest/router.go
+++ internal/delivery/rest/router.go
@@ -6,5 +6,6 @@ func LoadRoutes(e *echo.Echo, handler *handler) {
 	e.GET("/menu", handler.GetMenuList)
 	e.POST("/menu", handler.AddNewMenu)
 
+	e.GET("/order/:orderID", handler.GetOrderInfo)
 	e.POST("/order", handler.Order)
 }
